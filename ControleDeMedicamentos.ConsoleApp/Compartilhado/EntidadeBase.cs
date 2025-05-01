5fb1e21cfb6d6e84f52c0f76c758a1944fb6d558
Filipe Straubel <fstraubel10@gmail.com>
2025-05-01 14:29:27 -0300

    Add project files.

---
+++ ControleDeMedicamentos.ConsoleApp/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,12 @@
+﻿namespace ControleDeMedicamentos.ConsoleApp.Compartilhado
+{
+    public abstract class EntidadeBase<T>
+    {
+
+        public int Id { get; set; }
+
+        public abstract void AtualizarRegistro(T registroEditado);
+
+        public abstract string Validar();
+    }
+}
---
+++ ControleDeMedicamentos.ConsoleApp/Util/MenuPrincipal.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace ClubeDaLeitura2.ConsoleApp.Util
+{
+    public class MenuPrincipal
+    {
+        private char opcaoPrincipal;
+        
+
+        public MenuPrincipal()
+        {
+
+            
+            
+        }
+
+
+        public void ApresentarMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("|        Clube da Leitura         |");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("-----------------------------------");
+
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("1 - Gerenciar Amigos");
+            Console.WriteLine("2 - Gerenciar Caixas");
+            Console.WriteLine("3 - Gerenciar Revistas");
+            Console.WriteLine("4 - Gerenciar Emprestimos");
+            Console.WriteLine("S - Sair");
+
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+            Console.ResetColor();
+            opcaoPrincipal = Console.ReadLine()[0];
+
+            
+
+        }
+
+        public ITelaCrud ObterTela()
+        {
+            if (opcaoPrincipal == '1')
+                
+
+            return null;
+        }
+
+    }
+}
