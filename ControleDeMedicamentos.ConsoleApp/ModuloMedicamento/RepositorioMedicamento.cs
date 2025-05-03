f025cb9611f1dd6fcf8f87b3ecc17dc0541783e6
Philipi Sauer <philipisauer09@gmail.com>
2025-05-03 15:35:23 -0300

    criado CRUD repositorio de medicamentos

---
+++ ControleDeMedicamentos.ConsoleApp/ModuloMedicamento/RepositorioMedicamento.cs
@@ -0,0 +1,17 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedor;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloMedicamento
+{
+        public class RepositorioMedicamento : RepositorioBase<Medicamento>, IRepositorioMedicamento
+        {
+            public RepositorioMedicamento(ContextoDeDados contexto) : base(contexto)
+            {
+            }
+
+            protected override List<Medicamento> ObterRegistros()
+            {
+                return contexto.Medicamentos;
+            }
+        }
+}
---
+++ ControleDeMedicamentos.ConsoleApp/ModuloMedicamento/TelaMedicamento.cs
@@ -0,0 +1,41 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedor;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloMedicamento
+{
+    public class TelaMedicamento : TelaBase<Medicamento>, ITelaCrud
+    {
+        public TelaMedicamento(IRepositorioMedicamento repositorio) : base("Medicamento", repositorio)
+        {
+        }
+        public override Medicamento ObterDados()
+        {
+            Console.WriteLine("Digite o Nome do medicamento: ");
+            string nomeMedicamento = Console.ReadLine()!;
+
+            Console.WriteLine("Digite a Descrição do medicamento: ");
+            string descricao = Console.ReadLine()!;
+
+            Console.WriteLine("Digite a quantidade do medicamento: ");
+            string quantidade = Console.ReadLine()!;
+
+            Medicamento medicamento = new Medicamento(nomeMedicamento, descricao, quantidade);
+
+            return medicamento;
+        }
+
+        public override void ExibirCabecalhoTabela()
+        {
+            Console.WriteLine("{0, -10} | {1, -30} | {2, -20} | {3, -30}",
+                "Id", "Medicamento", "Descricao", "Quantidade");
+        }
+
+        public override void ExibirLinhaTabela(Medicamento medicamento)
+        {
+            Console.WriteLine("{0, -10} | {1, -30} | {2,-20} | {3, -30}", 
+                medicamento.Id, medicamento.NomeMedicamento, medicamento.Descricao, medicamento.Quantidade);
+        }
+
+        
+    }
+}
