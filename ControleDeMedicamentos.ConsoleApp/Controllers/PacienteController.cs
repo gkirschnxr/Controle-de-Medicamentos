f62251c8e0f72c7acf7246da21362e2ff6eaab56
gkirschnxr <kirschnxr@gmail.com>
2025-05-26 19:57:19 -0300

    Atribuído MVC para Visualizar Pacientes

--- ControleDeMedicamentos.ConsoleApp/Controllers/PacienteController.cs
+++ ControleDeMedicamentos.ConsoleApp/Controllers/PacienteController.cs
@@ -1,6 +1,27 @@
-﻿namespace ControleDeMedicamentos.ConsoleApp.Controllers
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Models;
+using ControleDeMedicamentos.ConsoleApp.ModuloPaciente;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeMedicamentos.ConsoleApp.Controllers;
+
+[Route("/pacientes")]
+public class PacienteController : Controller
 {
-    public class PacienteController
-    {
+    private ContextoDeDados contexto;
+    private IRepositorioPaciente repositorioPaciente;
+    
+    public PacienteController() {
+        contexto = new ContextoDeDados(true);
+        repositorioPaciente = new RepositorioPaciente(contexto);
+    }
+
+    [HttpGet("visualizar")]
+    public IActionResult Visualizar() {  
+       var pacientes = repositorioPaciente.SelecionarRegistros();
+
+       var visualizarVM = new VisualizarPacienteViewModel(pacientes);
+        
+       return View(visualizarVM); 
     }
 }
